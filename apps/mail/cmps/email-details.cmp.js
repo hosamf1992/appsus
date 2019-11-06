a87290eca4aef6a6c3cdee37090d22f94afa974f
hosam faour <hosamf1992@gmail.com>
2019-11-06 15:57:57 +0200

    filter

---
+++ apps/mail/cmps/email-details.cmp.js
@@ -0,0 +1,17 @@
+
+export default {
+    props: ['email'],
+    template: `
+        <li class="flex space-around">
+            <h1> {{email.sentFrom}}</h1>
+            <h1> {{email.subject}}</h1>
+            <p>time</p>
+          
+        </li>
+    `,
+    
+
+    computed:{
+       
+    },
+}
