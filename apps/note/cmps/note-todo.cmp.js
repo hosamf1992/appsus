79c98544aafb6f63878a9b6f107754185824e26e
Shachal Ami-Ad <shachalamiad@hotmail.com>
2019-11-07 11:10:04 +0200

    bind todo class

--- apps/note/cmps/note-todo.cmp.js
+++ apps/note/cmps/note-todo.cmp.js
@@ -4,16 +4,24 @@ import { noteService } from "../services/note-service.js"
 export default {
   template: `
         <section>
-          <h1 :toDoTxts="splitValue" v-for="txt in splitValue">{{txt}}</h1>
+          <h1 class="toDo" :toDoTxts="splitValue" v-for="toDoTxt in splitValue" 
+          @click="isActive = !isActive" :class="{active: isActive}">{{toDoTxt}}</h1>
             <button @click="removeNote(value.id)">x</button>
         </section>
         `,
   props: ["value"],
   
+
+  data() {
+    return {
+            isActive: false
+    }
+},
   methods: {
     removeNote(noteId) {
       noteService.removeNote(noteId)
-  }
+  },
+    
     },
   computed: {
     splitValue() {
